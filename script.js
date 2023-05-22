14ee66bc4157eac4f76dd33152637d9b9b37130c
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-22 10:11:52 -0300

    todos exercicios completos
    com lint

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const submitBtn = document.querySelector('#submit-btn');
 const agreement = document.querySelector('#agreement');
-// Requisito 4 //
+// gitRequisito 4 //
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
 });
 
 // Requisito 5 //
-agreement.addEventListener('change', (event) => {
+agreement.addEventListener('change', () => {
   submitBtn.disabled = false;
-  
-})
+});
 const btnCondition = () => {
   if (!agreement.onchange) {
     submitBtn.disabled = true;
