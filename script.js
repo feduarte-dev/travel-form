e8879f6013f040d3aac1f5a10f4bb1476433fe5e
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-22 10:06:38 -0300

    todos exercicios completos

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const submitBtn = document.querySelector('#submit-btn');
-
+const agreement = document.querySelector('#agreement');
+// Requisito 4 //
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
 });
+
+// Requisito 5 //
+agreement.addEventListener('change', (event) => {
+  submitBtn.disabled = false;
+  
+})
+const btnCondition = () => {
+  if (!agreement.onchange) {
+    submitBtn.disabled = true;
+  }
+};
+
+btnCondition();
